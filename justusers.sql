686b01e290609a9d6ec6724d1a998e96296ec6fb
Abdulrahman-Haji <77126546+ENG-CJ@users.noreply.github.com>
2022-09-03 20:26:34 +0300

    Add files via upload

---
+++ justusers.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 03, 2022 at 07:25 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `justusers`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_user` (IN `$username` VARCHAR(100), IN `$email` VARCHAR(160), IN `$pass` VARCHAR(100), IN `$role` VARCHAR(100))   BEGIN
+
+INSERT INTO `users`(`Username`, `Email`, `Password`, `Role`)
+VALUES ($username,$email,$pass,$role);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `searchUser` (IN `$email` VARCHAR(160), IN `$pass` INT(100))   BEGIN
+ SELECT *FROM users
+ WHERE users.Email=$email AND users.Password=$pass;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `Username` varchar(100) NOT NULL,
+  `Email` varchar(160) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `Role` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID`, `Username`, `Email`, `Password`, `Role`) VALUES
+(1, 'abdulrahman', 'abdulrahmandev10@gmail.com', '9090', 'admin'),
+(2, 'nasra', 'nasra@gmail.com', '9090', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
